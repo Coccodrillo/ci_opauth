227cd8929538e2079068fcd199a213d2f257d672
Destino Multimedia <destinomultimedia@gmail.com>
2012-05-31 19:10:42 +0300

    Update application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ci_opauth/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
